db17d368b9ce2ed66159c311addaadfbc5ed187f
albonec <agbonecchi@gmail.com>
2022-12-04 12:03:28 -0800

    Day 4 Part 2

--- src/CampCleanup.java
+++ src/CampCleanup.java
@@ -1,6 +1,5 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CampCleanup {
@@ -13,9 +12,14 @@ public class CampCleanup {
         }
     }
 
+    public static boolean doRangesIntersect(int min1, int max1, int min2, int max2) {
+        return !(max1 < min2 || max2 < min1);
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new FileReader("res/campcleanup.txt"));
-        int count = 0;
+        int countEncapsulate = 0;
+        int countIntersect = 0;
         while(scan.hasNextLine()) {
             String line = scan.nextLine();
             String half1 = line.substring(0, line.indexOf(','));
@@ -24,15 +28,15 @@ public class CampCleanup {
             int num2 = Integer.parseInt(half1.substring(half1.indexOf("-") + 1));
             int num3 = Integer.parseInt(half2.substring(half2.indexOf(",") + 1, half2.indexOf("-")));
             int num4 = Integer.parseInt(half2.substring(half2.indexOf("-") + 1));
-            System.out.println(line);
             if(doRangesCover(num1, num2 ,num3 ,num4)) {
-                System.out.println(true);
-                count++;
-            } else {
-                System.out.println(false);
+                countEncapsulate++;
+            }
+            if(doRangesIntersect(num1, num2, num3, num4)) {
+                countIntersect++;
             }
         }
-        System.out.println(count + " intervals fully cover the other");
+        System.out.println("Part 1: " + countEncapsulate + " intervals fully cover the other");
+        System.out.println("Part 2: " + countIntersect + " intervals intersect each other");
     }
 
 }
